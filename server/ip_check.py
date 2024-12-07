a6a92c839d3d960374aa1e435f609766d6ba1b68
PyAiBrain <pythonai.drive@gmail.com>
2024-12-07 14:29:16 +0100

    Update ip_check.py

--- server/ip_check.py
+++ server/ip_check.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 status_data = {"status": "kein-problem"}  # Default Status
 
-@app.route('/check-ip', methods=['POST'])
+@app.route('/check-ip', methods=['POST', 'GET'])
 def check_ip():
     client_ip = request.remote_addr
     if client_ip == "2.202.105.245":  # Beispiel-IP prüfen
         return jsonify({"status": status_data["status"], "admin": True})
     else:
-        return jsonify({"status": status_data["status"], "admin": False})
+        return jsonify({"status": client_ip, "admin": False})
 
 
 @app.route('/change-status', methods=['POST'])
