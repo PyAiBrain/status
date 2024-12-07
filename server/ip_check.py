3a520a8bddddd17888f7d598b3fe779b44ce0f11
PyAiBrain <pythonai.drive@gmail.com>
2024-12-07 14:08:32 +0100

    Update ip_check.py

--- server/ip_check.py
+++ server/ip_check.py
@@ -10,7 +10,7 @@ def check_ip():
     if client_ip == "2.202.105.245":  # Beispiel-IP prüfen
         return jsonify({"status": status_data["status"], "admin": True})
     else:
-        return jsonify({"status": status_data["status"], "admin": False})
+        return jsonify({"status": client_ip, "admin": False})
 
 
 @app.route('/change-status', methods=['POST'])
